bbb7e69e4aeed7ba6eef82f7d7f1420016945c5f
sasha <sasha@DESKTOP-SVQSLF1>
2016-12-19 21:15:41 +0200

    Добавьте файлы проекта.

---
+++ GameStore.Domain/Entities/ShippingDetails.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GameStore.Domain.Entities
+{
+    public class ShippingDetails
+    {
+        [Required(ErrorMessage = "Вкажiть ваше iм'я")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Вставте першу адресу доставки")]
+        [Display(Name = "Перша адреса")]
+        public string Line1 { get; set; }
+
+        [Display(Name = "Друга адреса")]
+        public string Line2 { get; set; }
+
+        [Display(Name = "Третя адреса")]
+        public string Line3 { get; set; }
+
+        [Required(ErrorMessage = "Вкажiть мiсто")]
+        [Display(Name = "Мiсто")]
+        public string City { get; set; }
+
+        [Required(ErrorMessage = "Вкажiть країну")]
+        [Display(Name = "Країна")]
+        public string Country { get; set; }
+
+        public bool GiftWrap { get; set; }
+
+    }
+}
